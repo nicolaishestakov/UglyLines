05d876a6030ace4aa66357159c88419d9a42e95c
Nikolay Shestakov <nikolay.shestakov@hexagon.com>
2022-04-04 14:24:17 +0700

    more Field tests

--- UglyLines.Tests/FieldTest.cs
+++ UglyLines.Tests/FieldTest.cs
@@ -36,6 +36,98 @@ public class FieldTest
             _field.Invoking(f => f.GetBall(xy)).Should().Throw<ArgumentOutOfRangeException>();
         }
     }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(4, 4)]
+    [InlineData(1, 2)]
+    [InlineData(0, 3)]
+    public void CantPlaceOnOccupiedCell(int x, int y)
+    {
+        var ball = new BallStub();
+        var xy = new Location(x, y);
+
+        _field.GetBall(xy).Should().BeNull();
+        _field.AddBall(new BallXY(ball, xy));
+        _field.Invoking(f => f.AddBall(new BallXY(new BallStub(), xy))).Should().Throw<ArgumentException>();
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(4, 4)]
+    [InlineData(1, 2)]
+    [InlineData(0, 3)]
+    public void AddBallEvent(int x, int y)
+    {
+        var ball = new BallStub();
+        var xy = new Location(x, y);
+        
+        using var monitoredField = _field.Monitor();
+
+        _field.BallAdded += (sender, args) =>
+        {
+            args.BallXy.Ball.Should().Be(ball);
+            args.BallXy.Location.Should().BeEquivalentTo(new Location(x, y));
+        };
+        
+        _field.AddBall(new BallXY(ball, xy));
+
+        monitoredField.Should().Raise("BallAdded");
+    }
+    
+    [Theory]
+    [InlineData(0, 0, 1, 1)]
+    [InlineData(4, 4, 0, 0)]
+    [InlineData(1, 2, 2, 3)]
+    [InlineData(0, 3, 3, 0)]
+    public void MoveBallEvent(int fromX, int fromY, int toX, int toY)
+    {
+        var ball = new BallStub();
+        var xy = new Location(fromX, fromY);
+        
+        using var monitoredField = _field.Monitor();
+        
+        _field.AddBall(new BallXY(ball, xy));
+
+        _field.BallMoved += (sender, args) =>
+        {
+            args.Ball.Should().Be(ball);
+            args.From.Should().BeEquivalentTo(new Location(fromX, fromY));
+            args.To.Should().BeEquivalentTo(new Location(toX, toY));
+        };
+        
+        _field.MoveBall(xy, new Location(toX, toY));
+        
+        monitoredField.Should().Raise("BallMoved");
+    }
+
+    [Fact]
+    public void ClearBalls()
+    {
+        var balls = new[]
+        {
+            new BallXY(new BallStub(), new Location(0, 0)),
+            new BallXY(new BallStub(), new Location(4, 4)),
+            new BallXY(new BallStub(), new Location(1, 1)),
+            new BallXY(new BallStub(), new Location(0, 3)),
+        };
+
+        foreach (var ballXy in balls)
+        {
+            _field.AddBall(ballXy);
+        }
+
+        _field.BallsRemoved += (sender, args) =>
+        {
+            args.RemovedBalls.Should().BeEquivalentTo(balls);
+        };
+        using var monitoredField = _field.Monitor();
+        
+        _field.Clear();
+        _field.GetBalls().Should().BeEmpty();
+        
+        monitoredField.Should().Raise("BallsRemoved");
+    }
     
     private class BallStub : IBall
     {
